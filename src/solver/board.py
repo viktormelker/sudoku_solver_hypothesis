0edbcefbdf7d02d938bdae006ffa5d4155d0f234
Viktor Melker <viktormelker@gmail.com>
2023-06-28 10:56:26 +0200

    Add convenience methods on the sudoku board

--- src/solver/board.py
+++ src/solver/board.py
@@ -15,6 +15,29 @@ class PositionY(Enum):
     BOTTOM = auto()
 
 
+def to_position_x(position: int) -> PositionX:
+    match position:
+        case 0 | 1 | 2:
+            return PositionX.LEFT
+        case 3 | 4 | 5:
+            return PositionX.MIDDLE
+        case 6 | 7 | 8:
+            return PositionX.RIGHT
+        case _:
+            raise ValueError(f"Value {position} was not allowed, must be 0-8")
+
+def to_position_y(position: int) -> PositionY:
+    match position:
+        case 0 | 1 | 2:
+            return PositionY.TOP
+        case 3 | 4 | 5:
+            return PositionY.MIDDLE
+        case 6 | 7 | 8:
+            return PositionY.BOTTOM
+        case _:
+            raise ValueError(f"Value {position} was not allowed, must be 0-8")
+
+
 @dataclass
 class Cell:
     value: Optional[int] = None
@@ -25,13 +48,24 @@ class Cell:
 class SudokuBoard:
     board: List[List[Cell]]
 
+    @property
+    def rows(self):
+        return self.board
+
+    @property
+    def columns(self):
+        num_columns = len(self.board[0])
+        return [
+            [self.board[row_idx][col_idx] for row_idx in range(len(self.board))]
+            for col_idx in range(num_columns)
+        ]
+
     def is_solution_valid(self) -> bool:
-        for row in self.board:
+        for row in self.rows:
             if not self.is_group_valid(row):
                 return False
 
-        for col in range(9):
-            column = self.get_column(col)
+        for column in self.columns:
             if not self.is_group_valid(column):
                 return False
 
@@ -57,12 +91,6 @@ class SudokuBoard:
             seen_values.add(cell.value)
 
         return True
-
-    def get_column(self, col: int) -> List[Cell]:
-        return [self.board[row][col] for row in range(9)]
-
-    def get_row(self, row: int) -> List[Cell]:
-        return self.board[row]
 
     def get_box(self, position_y: PositionY, position_x: PositionX):
         start_row = (position_y.value - 1) * 3
